5886d3b33aafad83ccdca256f0208c67a6a2950c
Miquarll <steven.altamirano.work@gmail.com>
2019-05-30 09:48:19 +0300

    locoSide added

--- src/task4/model/train/PassengerTrain.java
+++ src/task4/model/train/PassengerTrain.java
@@ -3,14 +3,16 @@ package task4.model.train;
 import task4.model.passenger.Person;
 
 public class PassengerTrain extends Train {
+    // This means at which side locomotive is located
     private LocoSide locoSide;
 
     public enum LocoSide{
         LEFT,
-        RIDE
+        RIGHT
     }
 
-    public PassengerTrain(Person driver) {
+    public PassengerTrain(Person driver, LocoSide locoSide) {
         super(driver);
+        this.locoSide = locoSide;
     }
 }
